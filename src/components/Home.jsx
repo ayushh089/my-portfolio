ac276429774e7b3fa1961708b693bda4d4b67843
aayu17 <nextcoders.89@gmail.com>
2024-08-12 11:16:16 +0530

    d

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="relative h-[100vh] w-full">
+    <div className="relative min-h-screen w-full flex items-center justify-center">
       <div className="absolute inset-0 bg-[url('https://www.rammaheshwari.com/assets/svg/common-bg.svg')] bg-cover bg-center opacity-20"></div>
-      <div className="absolute left-1/2 transform -translate-x-1/2 p-4 sm:p-6 md:p-8 lg:p-12 top-[20%] sm:top-[25%] md:top-[30%] ">
-        <div className="text-black font-extrabold text-[2rem] sm:text-[2.5rem] md:text-[3rem] lg:text-[4rem] uppercase text-center tracking-[3px] sm:tracking-[4px] lg:whitespace-nowrap">
+      <div className="relative z-10 p-4 sm:p-6 md:p-8 lg:p-12 max-w-4xl mx-auto">
+        <div className="text-black font-extrabold text-3xl sm:text-3xl md:text-4xl lg:text-5xl uppercase text-center tracking-wider sm:tracking-widest break-words">
           Hey, I'm Ayush Gupta
         </div>
-        <p className="text-sm sm:text-base md:text-lg lg:text-xl mt-6 text-center max-w-[80%] mx-auto">
+        <p className="text-sm sm:text-base md:text-lg lg:text-xl mt-4 sm:mt-6 text-center mx-auto">
           A Result-Oriented Web Developer building and managing Websites and Web Applications that lead to the success of the overall product
         </p>
       </div>
